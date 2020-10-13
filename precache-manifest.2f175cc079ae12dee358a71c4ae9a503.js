f0d1fb3b34ea167215d5d2a24b219be21c8d21d0
shizuku <2112165916@qq.com>
2020-10-13 12:40:42 +0800

    Updates

---
+++ precache-manifest.2f175cc079ae12dee358a71c4ae9a503.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a4c3fcb580839e4426da78fd68fb0be",
+    "url": "/calc-react/index.html"
+  },
+  {
+    "revision": "a657450f5a55928d707b",
+    "url": "/calc-react/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "133f234d07aa54cb8fde",
+    "url": "/calc-react/static/css/main.753ac29a.chunk.css"
+  },
+  {
+    "revision": "a657450f5a55928d707b",
+    "url": "/calc-react/static/js/2.fe57d1c1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/calc-react/static/js/2.fe57d1c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "133f234d07aa54cb8fde",
+    "url": "/calc-react/static/js/main.3807b5e9.chunk.js"
+  },
+  {
+    "revision": "887129de4f74fc2e43be",
+    "url": "/calc-react/static/js/runtime-main.45110a7c.js"
+  }
+]);
